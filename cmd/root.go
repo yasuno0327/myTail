f68af452aeb6cf850ad223f6afa66668e1b21b2f
yasuno0327 <5315076@ed.tus.ac.jp>
2018-09-23 17:57:46 +0900

    Separate file

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"sync"
@@ -43,42 +42,6 @@ func AnalyzeArgument(cmd *cobra.Command, args []string) {
 		}(args[i])
 	}
 	wg.Wait()
-}
-
-// 最後からn行分だけファイルの中身をプリントする
-func PrintFileN(n int, filename string, wd string) {
-	file, err := os.Open(wd + "/" + filename)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	// 最初にある程度容量を確保する(append高速化)
-	lines := make([]string, 0, 1000)
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	length := len(lines)
-	if n > length {
-		n = length
-	}
-	head := length - n // printする先頭行
-	mutex.Lock()       // printの間だけ同期ロック
-	fmt.Printf("==> %s <==\n", filename)
-	for i := 0; i < n && head < length; i++ {
-		output := lines[head]
-		fmt.Printf("%s\n", output)
-		head++
-	}
-	fmt.Println("")
-	mutex.Unlock()
 }
 
 func Execute() {
